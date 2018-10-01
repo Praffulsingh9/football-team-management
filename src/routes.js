23113ceab3374f0f0b856d09b74be9f8f8a3ecd3
Praffulsingh9 <prafful.singh017@gmail.com>
2018-10-01 17:55:09 +0530

    Stripe Animation Added

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
-
+import {Route,Switch} from 'react-router-dom';
+import Home from './components/home';
 const Routes =(props) => {
    return (
      <Layout>
-       
+       <Switch>
+         <Route  exact path="/" component={Home}/>
+       </Switch>
      </Layout>
    );
 }
