5916c5c8273f2f2fa6fa815dd6bef284ff23c499
Praffulsingh9 <prafful.singh017@gmail.com>
2018-10-01 17:03:09 +0530

    Header and Footer components added

--- src/components/Header_Footer/Footer.js
+++ src/components/Header_Footer/Footer.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import {CityLogo} from '../ui/icons';
 
 class Footer extends Component {
     render() {
         return (
-            <div>
-                Footer
-            </div>
+            <footer className="bck_blue">
+                <div className="footer_logo">
+                    <CityLogo width="70px" height="70px" link={true} linkTo="/"/>
+                </div>
+                <div className="footer_disclamer">
+                  Manchester City 2018.All Rights reserved.
+                </div>
+            </footer>
         );
     }
 }
