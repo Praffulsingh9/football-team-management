35a03df21f6fd9503ac095936e32a07c6443417b
Prafful Singh <prafful.singh017@gmail.com>
2018-10-02 23:59:40 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import 'firebase/app';
 import 'firebase/database';
 import 'firebase/storage';
 
+// Get your credentials feom firebase
 const config = {
-    apiKey: "AIzaSyDrp4nZIGWC4LLW7Hysuul2I7I9Be4JffE",
-    authDomain: "m-city-e891f.firebaseapp.com",
-    databaseURL: "https://m-city-e891f.firebaseio.com",
-    projectId: "m-city-e891f",
-    storageBucket: "m-city-e891f.appspot.com",
-    messagingSenderId: "896990184268"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
 
   firebase.initializeApp(config);
