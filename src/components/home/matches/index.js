0dbbf7bd9f441e79327d151fbcac0417aeb3f7f2
Praffulsingh9 <prafful.singh017@gmail.com>
2018-10-02 22:52:47 +0530

    Matches section of Homepage Intialised

---
+++ src/components/home/matches/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Tag} from '../../ui/misc';
+const MatchesHome = () => {
+    return (
+        <div className="home_matches_wrapper">
+            <div className="container">
+            <Tag bck="#0e1731" size="50px" color="white" >Matches</Tag>
+
+            BLOCKS 
+
+            <Tag link={true} linkTo='/matches' bck="#ffffff" size="22px" color="#0e1731">See more matches</Tag>
+            
+            </div>
+        </div>
+    );
+};
+
+export default MatchesHome;
