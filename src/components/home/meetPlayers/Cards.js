953ba86d869adae8ba00f44a760fae71a4e389a2
Praffulsingh9 <prafful.singh017@gmail.com>
2018-10-06 01:25:46 +0530

    Meet the Players components finished

--- src/components/home/meetPlayers/Cards.js
+++ src/components/home/meetPlayers/Cards.js
@@ -1,59 +1,71 @@
 import React, { Component } from 'react';
-import {easePolyOut} from 'd3-ease';
+import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
 import Otamendi from '../../../Resources/images/players/Otamendi.png';
+import PlayerCard from '../../ui/playerCard';
+
 
 class HomeCards extends Component {
 
-   state = {
-       
-       cards:[
-           {
-               bottom:90,
-               left:300
-           },
-           {
-            bottom:60,
-            left:200
-           },
-           {
-            bottom:30,
-            left:100
-           },
-           {
-            bottom:0,
-            left:0
-           }
-       ]
-   }
+    state = {
+        cards:[
+            {
+                bottom: 90,
+                left: 300
+            },
+            {
+                bottom: 60,
+                left: 200
+            },
+            {
+                bottom: 30,
+                left: 100
+            },
+            {
+                bottom: 0,
+                left: 0
+            }
+        ]
+    }
 
-   showAnimateCards = () => {
+    showAnimateCards = () => (
         this.state.cards.map((card,i)=>(
-             <Animate
-               key={i}
-               start={{
-                   left:0,
-                   bottom:0
-               }}
+            <Animate
+                key={i}
+                show={this.props.show}
 
-               enter = {{
-                   left:[card.left],
-                   bottom:[card.bottom],
-                   timing:{duration:500,ease:easePolyOut}
-               }}
-               show={this.props.show}>
-                 {({left,bottom})=>{
-                     return (
-                         <div style={{positon:'absolute',
-                         left,
-                         bottom}}>
+                start={{
+                    left:0,
+                    bottom:0
+                }}
 
-                         </div>
-                     )
-                 }}
-             </Animate>
+                enter={{
+                    left: [card.left],
+                    bottom: [card.bottom],
+                    timing: {duration: 500, ease: easePolyOut}
+                }}
+            >
+                {({ left , bottom })=>{
+                    return(
+                        <div
+                            style={{
+                                position: 'absolute',
+                                left,
+                                bottom
+                            }}
+                        >
+                            <PlayerCard
+                                number="30"
+                                name="Nicolas"
+                                lastname="Otamendi"
+                                bck={Otamendi}
+                            />
+                        </div>
+                    )
+                }}
+            </Animate>
         ))
-   }
+    )
 
     render() {
         return (
