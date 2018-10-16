7b43c0e1f602ae711029843665f2f4eab0933965
Praffulsingh9 <prafful.singh017@gmail.com>
2018-10-16 23:27:53 +0530

    Promotion Component Initialised

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Featured from './featured';
 import Matches from './matches';
 import MeetPlayers from './meetPlayers/index';
-
+import Promotion from './promotion/index';
 const Home = () => {
     return (
         <div className="bck_blue">
             <Featured/>
             <Matches />
             <MeetPlayers />
+            <Promotion />
         </div>
     );
 };
