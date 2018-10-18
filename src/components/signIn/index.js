ba2978e2ac9212895b172bb4c8a59a5fb02eaaff
Praffulsingh9 <prafful.singh017@gmail.com>
2018-10-18 21:01:12 +0530

    SignIn Component Finished

--- src/components/signIn/index.js
+++ src/components/signIn/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FormField from '../ui/formFields';
+import {firebase} from '../../firebase';
 import { validate } from '../ui/misc';
+
 class SignIn extends Component {
 
     state = {
@@ -52,10 +54,21 @@ class SignIn extends Component {
         }
 
         if(formIsValid){
-           
+           firebase.auth()
+           .signInWithEmailAndPassword(
+               dataToSubmit.email,
+               dataToSubmit.password
+           )
             } else {
             this.setState({
                 formError: true
+            })
+            .then(()=>{
+                this.props.history.push('/dashboard')
+            }).catch(error=>{
+                this.setState({
+                    formError:true
+                })
             })
         }
     }
@@ -96,6 +109,11 @@ class SignIn extends Component {
                             formdata={this.state.formdata.password}
                             change={(element)=> this.updateForm(element)}
                         />
+
+                        { this.state.formError ? 
+                                <div className="error_label">Something is wrong, try again.</div>
+                                :null
+                            }
 
                         <button onClick={(event)=> this.submitForm(event)}>Log In</button>
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/app'
 import 'firebase/database'
+import 'firebase/auth'
 
 const config = {
     apiKey: "AIzaSyDrp4nZIGWC4LLW7Hysuul2I7I9Be4JffE",
