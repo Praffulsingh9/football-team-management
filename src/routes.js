e63f5f7f5c8f7421350e77e341a500a154f4d1a1
Praffulsingh9 <prafful.singh017@gmail.com>
2018-10-19 00:53:49 +0530

    Dashboard matches component added

--- src/routes.js
+++ src/routes.js
@@ -10,11 +10,13 @@ import Dashboard from './components/admin/Dashboard';
 import PrivateRoute from './components/authRoutes/privateRoutes';
 import PublicRoute from './components/authRoutes/publicRoutes';
 
+import AdminMatches from './components/admin/matches/Index';
+
 const Routes = (props) => {
   return(
     <Layout>
         <Switch>
-
+            <PrivateRoute {...props} exact component={AdminMatches} path="/admin_matches"/>
             <PrivateRoute {...props} exact component={Dashboard} path="/dashboard"/>
             <PublicRoute {...props} restricted={true} exact component={SignIn} path="/sign_in"/>       
             <PublicRoute {...props} restricted={false} exact component={Home} path="/"/>   
